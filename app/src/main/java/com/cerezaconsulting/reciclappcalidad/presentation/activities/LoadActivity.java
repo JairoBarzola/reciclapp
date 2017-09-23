c041d9c107252f6dd173395da9160d3ec8740278
JairTouchMobile <jairobc16@gmail.com>
2017-09-23 18:18:26 -0500

    Ok

--- app/src/main/java/com/cerezaconsulting/reciclappcalidad/presentation/activities/LoadActivity.java
+++ app/src/main/java/com/cerezaconsulting/reciclappcalidad/presentation/activities/LoadActivity.java
@@ -2,10 +2,14 @@ package com.cerezaconsulting.reciclappcalidad.presentation.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
 
 import com.cerezaconsulting.reciclappcalidad.R;
 import com.cerezaconsulting.reciclappcalidad.core.BaseActivity;
 import com.cerezaconsulting.reciclappcalidad.data.repositories.local.SessionManager;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by miguel on 16/05/17.
@@ -19,7 +23,25 @@ public class LoadActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loader);
         sessionManager = new SessionManager(getApplicationContext());
-        initializeView();
+
+        Thread t = new Thread(){
+            public void run(){
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    initializeView();
+                }
+            }
+        };
+        t.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 
     private void initializeView(){
